beb8d5a5de01baa55d41f800c2a8426c230df0cb
diegotrazos <torradosanchezdiego@gmail.com>
2024-06-17 19:13:45 +0200

    Update

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import Planets from '../pages/Planets/Planets';
+import { PLANET_DATA } from '../constants/planet-data';
+import { v4 } from 'uuid';
 
 const Router = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<h1>APP</h1>} />
+			{PLANET_DATA.map(planetData => (
+				<Route
+					key={v4()}
+					path={planetData.path}
+					element={<Planets planetData={planetData} />}
+				/>
+			))}
 		</Routes>
 	);
 };
